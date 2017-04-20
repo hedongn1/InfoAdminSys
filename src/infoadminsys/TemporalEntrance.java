a7a5ddcea8276e5b680eaa54d60f705d84f3d8cd
lsh <lshzy137@gmail.com>
2017-04-20 16:03:09 +0800

    The former merging havs bugs. Remerged.

--- src/infoadminsys/TemporalEntrance.java
+++ src/infoadminsys/TemporalEntrance.java
@@ -15,30 +15,9 @@ import javax.swing.WindowConstants;
 public class TemporalEntrance {
     
     public static void main(String args[]) {
-<<<<<<< HEAD
-=======
-        
->>>>>>> 9f92e45fd14d6fc6d8b73da1fd0423b21f9cd39b
-        /*
-        StudentUI S = new StudentUI("15307130120");
-        S.setLocationRelativeTo(null);
-        S.setVisible(true);
-        
-        AccountManageUI AM=new AccountManageUI("15307130120","学生");
-        AM.setLocationRelativeTo(null);
-        AM.setVisible(true);
-        */
-<<<<<<< HEAD
         AdminUI AD=new AdminUI();
         AD.setLocationRelativeTo(null);
         AD.setVisible(true);
-=======
->>>>>>> 9f92e45fd14d6fc6d8b73da1fd0423b21f9cd39b
-        
-        TeacherUI T = new TeacherUI("123");
-        T.setLocationRelativeTo(null);
-        T.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        T.setVisible(true);
     }
     
 }
